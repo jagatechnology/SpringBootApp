21f26d2d93713b486b55c5892caa2ec33c001cf1
kalyani <kalyanibeura@gmail.com>
2021-03-15 13:42:51 +0530

    This is first commit done by kalyani

---
+++ src/com/kalyanigit/raghusir/MessageRunner.java
@@ -0,0 +1,13 @@
+package com.kalyanigit.raghusir;
+
+public class MessageRunner {
+
+	public static void main(String[] args) {
+		int i=10;
+		i=++i + i++ + ++i;
+		System.out.println("Jay Jagannath and welcome to my spring boot app.");
+		System.out.println("The value of i="+i);
+
+	}
+
+}
